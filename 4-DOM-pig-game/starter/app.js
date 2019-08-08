12f29b41797c497caaa9ab76af6aae1d794b8dbd
Jaysig <jasontsigmon@gmail.com>
2019-08-08 16:00:58 -0500

    Game now functions normally

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,41 +9,43 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlayer;
+let scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', () => {
+    if (gamePlaying) {
+        let dice = Math.floor(Math.random() * 6) + 1;
 
-    let dice = Math.floor(Math.random() * 6) + 1;
+        let diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = `dice-${dice}.png`;
 
-    let diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = `dice-${dice}.png`;
-
-    if (dice !== 1) {
-        roundScore += dice;
-        document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
-    } else {
-        nextPlayer();
+        if (dice !== 1) {
+            roundScore += dice;
+            document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
     }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', () => {
-    scores[activePlayer] += roundScore;
+    if (gamePlaying) {
+        scores[activePlayer] += roundScore;
 
-    document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+        document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
 
-    if (scores[activePlayer] >= 20) {
-        document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
-        document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
-    } else {
-        nextPlayer();
+        if (scores[activePlayer] >= 100) {
+            document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+            document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+            gamePlaying = false;
+        } else {
+            nextPlayer();
+        }
     }
-
-
 })
 
 function nextPlayer() {
@@ -80,11 +82,13 @@ function init() {
 
     document.querySelector(`.player-0-panel`).classList.remove('winner');
     document.querySelector(`.player-1-panel`).classList.remove('winner');
-    
+
     document.querySelector(`.player-0-panel`).classList.remove('active');
     document.querySelector(`.player-1-panel`).classList.remove('active');
 
     document.querySelector(`.player-0-panel`).classList.add('active');
+
+    gamePlaying = true;
 }
 
 
