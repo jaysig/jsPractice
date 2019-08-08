dd2e9fe0f36b4d66f15ef1f95878a77434e42f3e
Jaysig <jasontsigmon@gmail.com>
2019-08-08 15:24:46 -0500

    Dice now rolls properly

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -13,11 +13,26 @@ let scores = [0,0];
 let roundScore = 0;
 let activePlayer = 0;
 
-let dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector(`#current-${activePlayer}`).textContent = dice;
-// document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`;
-
-let x = document.querySelector('#score-0').textContent;
-
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+
+    let dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice)
+
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = `dice-${dice}.png`;
+
+
+})
+
+//document.querySelector(`#current-${activePlayer}`).textContent = dice;
+// document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`;
+//let x = document.querySelector('#score-0').textContent;
